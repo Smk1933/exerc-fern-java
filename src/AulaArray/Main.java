06958fa6a53a57d696c5703e981407e322d75250
Samuel Henrique Gomes de Medeiros <samuelhgg93@gmail.com>
2024-09-28 20:34:55 -0300

    testes aula fiasco

---
+++ src/AulaArray/Main.java
@@ -0,0 +1,22 @@
+package AulaArray;
+
+import Ternarios.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String [] ninja = new String[5];
+
+        ninja[0] = "Rock Lee";
+        ninja[1] = "Itachi Uchiha";
+        ninja[2] = "Layla Gang";
+
+
+
+        for (int i = 0; i < ninja.length ; i++) {
+            System.out.println(ninja[i]);
+
+        }
+
+    }
+}
